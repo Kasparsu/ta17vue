35bebaff5cc1898a3f593a5f99ddfb7900a66a5f
Oliver Steinbach <oliver.steinbach@tptlive.ee>
2020-03-15 13:14:04 +0200

    added autosave

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        {{ Math.floor(value)}}
+        <p>Clicks: {{ Number((value).toFixed(1))}}</p>
+        <p>Cps: {{Number((cps).toFixed(1))}}</p>
     </div>
 </template>
 
 <script>
     export default {
         name: "Counter",
-        props:['value']
+        props:['value', 'cps']
     }
 </script>
 
